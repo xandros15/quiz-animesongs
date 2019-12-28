d86a3f33ba5766e5369a7b512746f6f376a58df6
xandros15 <xandros92@gmail.com>
2019-12-28 23:26:16 +0100

    add mobile validator

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { mobileCheck } from '../tools/helpers'
 import actions from './actions'
 import game from './modules/game'
 import mutations from './mutations'
@@ -8,6 +9,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isMobile: mobileCheck(),
     auth: localStorage.auth || '',
     modal: '',
   },
