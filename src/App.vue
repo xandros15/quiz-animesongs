401e8bfd898e727380e05e3e22c727add01fa10e
xandros15 <xandros92@gmail.com>
2020-01-26 18:37:47 +0100

    set 1rem margin of top for app for avoid some collisions

--- src/App.vue
+++ src/App.vue
@@ -24,5 +24,6 @@
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+        padding-top: 1rem;
     }
 </style>
